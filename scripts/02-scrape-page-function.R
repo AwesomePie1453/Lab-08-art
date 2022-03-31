d147b6bb85e678e393a12dc9cade03436a3ed93d
AwesomePie1453 <alexconno@comcast.net>
2022-03-31 09:27:53 -0400

    i need to commit more

--- scripts/02-scrape-page-function.R
+++ scripts/02-scrape-page-function.R
@@ -3,25 +3,44 @@
 library(tidyverse)
 library(rvest)
 
+# trying functions ------------------------------------------------------------
+
+add_two <- function(x){
+  x + 2
+}
+
+add_two(3)
+
+add_two(10)
+
 # function: scrape_page --------------------------------------------------------
 
-___ <- function(url){
-  
-  # read page
-  page <- read_html(url)
-  
-  # scrape titles
-  titles <- ___
-  
-  # scrape links
-  links <- ___
-  
-  # scrape artists 
-  artists <- ___
-  
-  # create and return tibble
-  tibble(
-    ___
-  )
-  
+scrape_page <- function(url){
+page <- read_html(url)
+
+titles <-  page %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_text() %>%
+  str_squish()
+
+links <- page %>%
+  html_nodes(".iteminfo") %>%
+  html_node("h3 a") %>%
+  html_attr("href") %>%
+  str_replace("\\.", "https://collections.ed.ac.uk/art/")
+
+artists <- page %>%
+  html_nodes(".iteminfo") %>%
+  html_node(".artist") %>%
+  html_text() %>%
+  str_squish()
+
+tibble(
+  title = titles,
+  artist = artists,
+  link = links
+)
 }
+
+
